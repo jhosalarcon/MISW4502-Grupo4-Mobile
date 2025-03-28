cd35e998c689f8d3e5d1a5d41e426e389a67d0d4
jhosalarcon <ja.alarcon2@uniandes.edu.co>
2025-03-28 17:13:41 -0500

    :sparkles: CCP app init

---
+++ app/src/main/java/com/misw/gameralarm/Home.kt
@@ -0,0 +1,63 @@
+package com.misw.gameralarm
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class Home : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val etUsername: EditText = view.findViewById(R.id.etEmail)
+        val etPassword: EditText = view.findViewById(R.id.etPassword)
+        val btnLogin: Button = view.findViewById(R.id.btnLogin)
+        val btnCreateAccount: Button = view.findViewById(R.id.btnCreateAccount)
+        val tvForgotPassword: TextView = view.findViewById(R.id.tvForgotPassword)
+
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+            if (username.isNotEmpty() && password.isNotEmpty()) {
+                findNavController().navigate(R.id.action_home_to_dashboard)
+            } else {
+                showErrorDialog("Debe ingresar usuario y contraseña")
+            }
+        }
+
+        btnCreateAccount.setOnClickListener {
+            showPopup()
+        }
+
+        tvForgotPassword.setOnClickListener {
+            showErrorDialog("Funcionalidad no implementada")
+        }
+
+        return view
+    }
+
+    private fun showPopup() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Crear cuenta")
+        builder.setMessage("Esta funcionalidad no está en el alcance")
+        builder.setPositiveButton("Cerrar") { dialog, _ -> dialog.dismiss() }
+        builder.create().show()
+    }
+
+    private fun showErrorDialog(message: String) {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Error")
+        builder.setMessage(message)
+        builder.setPositiveButton("Aceptar") { dialog, _ -> dialog.dismiss() }
+        builder.create().show()
+    }
+}

70af4c689ccf8e9d132050ddc09494f7dde7f676
jhosalarcon <ja.alarcon2@uniandes.edu.co>
2025-04-06 20:07:06 -0500

    :sparkles: Add login

--- app/src/main/java/com/misw/gameralarm/network/ApiService.kt
+++ app/src/main/java/com/misw/gameralarm/network/ApiService.kt
@@ -2,6 +2,8 @@ package com.misw.gameralarm.data.network
 
 import com.misw.gameralarm.data.model.CrearCuentaRequest
 import com.misw.gameralarm.data.model.CrearCuentaResponse
+import com.misw.gameralarm.data.model.LoginRequest
+import com.misw.gameralarm.data.model.LoginResponse
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -9,4 +11,7 @@ import retrofit2.http.POST
 interface ApiService {
     @POST("auth/register")
     fun registerUser(@Body request: CrearCuentaRequest): Call<CrearCuentaResponse>
+
+    @POST("auth/login")
+    fun loginUser(@Body request: LoginRequest): Call<LoginResponse>
 }

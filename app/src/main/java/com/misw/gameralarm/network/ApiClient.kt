0faa060983f542add8ae7bae798d818853607fa5
jhosalarcon <ja.alarcon2@uniandes.edu.co>
2025-05-18 23:00:23 -0500

    :sparkles: Fix route

--- app/src/main/java/com/misw/gameralarm/network/ApiClient.kt
+++ app/src/main/java/com/misw/gameralarm/network/ApiClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object ApiClient {
     val apiService: ApiService by lazy {
         Retrofit.Builder()
-            .baseUrl("http://34.55.129.65:3000/")
+            .baseUrl("http://34.31.100.70:3000/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
